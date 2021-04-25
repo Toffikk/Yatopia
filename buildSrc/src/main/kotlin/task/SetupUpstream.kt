a60d8168d34fd53c4e656b654faa52b70b8084d1
Tofik <slodkitofik@gmail.com>
2021-04-25 16:37:23 +0200

    e

--- buildSrc/src/main/kotlin/task/SetupUpstream.kt
+++ buildSrc/src/main/kotlin/task/SetupUpstream.kt
@@ -17,7 +17,7 @@ internal fun Project.createSetupUpstreamTask(
     doLast {
         val setupUpstreamCommand = if (upstreamDir.resolve("scripts/build.sh").exists()) {
             if (System.getProperty("os.name").toLowerCase().startsWith("win")) {
-                "${toothpick.upstreamLowercase} patch"
+                "scripts/build.sh"
             } else {
                 "./${toothpick.upstreamLowercase} patch"
             }
