30d2f81642646a974acc911c8466641ee0853e3e
Tofik <slodkitofik@gmail.com>
2021-04-28 16:11:07 +0200

    emm

---
+++ gradle/buildSrc/src/main/kotlin/task/SetupUpstream.kt
@@ -0,0 +1,43 @@
+package task
+
+import bashCmd
+import gitHash
+import lastUpstream
+import org.gradle.api.Project
+import org.gradle.api.Task
+import taskGroup
+import toothpick
+import upstreamDir
+
+internal fun Project.createSetupUpstreamTask(
+    receiver: Task.() -> Unit = {}
+): Task = tasks.create("setupUpstream") {
+    receiver(this)
+    group = taskGroup
+    doLast {
+        val setupUpstreamCommand = if (upstreamDir.resolve("scripts/build.sh").exists()) {
+            if (System.getProperty("os.name").toLowerCase().startsWith("win")) {
+                "scripts/build.sh"
+            } else {
+                "./${toothpick.upstreamLowercase} patch"
+            }
+        } else if (
+                upstreamDir.resolve("build.gradle.kts").exists()
+                && upstreamDir.resolve("subprojects/server.gradle.kts").exists()
+                && upstreamDir.resolve("subprojects/api.gradle.kts").exists()
+        ) {
+            if (System.getProperty("os.name").toLowerCase().startsWith("win")) {
+                "gradlew applyPatches"
+            } else {
+                "./gradlew applyPatches"
+            }
+        } else {
+            error("Don't know how to setup upstream!")
+        }
+        val result = bashCmd(setupUpstreamCommand, dir = upstreamDir, printOut = true)
+        if (result.exitCode != 0) {
+            error("Failed to apply upstream patches: script exited with code ${result.exitCode}")
+        }
+        lastUpstream.writeText(gitHash(upstreamDir))
+    }
+}
