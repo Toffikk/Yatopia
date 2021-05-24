c95399f4655544b94c81e7b0a0f0a40be4260c31
tofikarz <62406295+tofikarz@users.noreply.github.com>
2021-05-24 09:05:41 +0200

    Update MakePatchesTask.java

--- buildSrc/src/main/java/org/yatopiamc/yatoclip/gradle/MakePatchesTask.java
+++ buildSrc/src/main/java/org/yatopiamc/yatoclip/gradle/MakePatchesTask.java
@@ -51,6 +51,8 @@ import java.util.zip.ZipFile;
 
 @DisableCachingByDefault
 public class MakePatchesTask extends DefaultTask {
+    
+     dependsOn(":yatopia-server:jar");
 
     @OutputDirectory
     private final File outputDir = ((Copy) getProject().getTasks().getByPath("processResources")).getDestinationDir().toPath().resolve("patches").toFile();
@@ -104,7 +106,6 @@ public class MakePatchesTask extends DefaultTask {
 
     @TaskAction
     public void genPatches() throws IOException, InterruptedException {
-            dependsOn(":yatopia-server:jar");
         Preconditions.checkNotNull(originalJar);
         Preconditions.checkNotNull(targetJar);
         getLogger().lifecycle("Generating patches for " + originalJar + " -> " + targetJar);
